d238fb7d8cbcc7c30df7f832839a901d4a0113d4
Vazha Kiknadze <74554787+kikna19@users.noreply.github.com>
2022-04-22 14:35:52 +0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyBtSYgxlm-3ao3nNLVO2BBViuqnFjlSREI",
-    authDomain: "notepad-42ad6.firebaseapp.com",
-    projectId: "notepad-42ad6",
-    storageBucket: "notepad-42ad6.appspot.com",
-    messagingSenderId: "1095418632390",
-    appId: "1:1095418632390:web:78241ca1d1e989fd3e5fc7",
-    measurementId: "G-MRJ2YJM4Q6"
-  }
 };
